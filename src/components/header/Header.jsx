e59be12fc55d9becc8a452b2c011e84420dd568c
remarkablejames <lascasasjames85@gmail.com>
2022-04-28 16:14:04 +0200

    enhanced the header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import "./header.css";
 import { CTA } from "./CTA";
-import ME from "../../assets/me.png";
+import ME from "../../assets/me.svg";
 import { Socials } from "./Socials";
 
 export const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, I'm</h5>
-        <h1>James Niyongira</h1>
-        <h5 className="text-light">A remarkable full stack developer</h5>
-        <CTA />
-        <Socials />
-
+        <div className="banner__container">
+          <h5>Hello, I'm</h5>
+          <h1>James Niyongira</h1>
+          <h5 className="text-light">
+            A remarkable full stack developer Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Unde ipsa modi, corrupti blanditiis
+            nulla earum fugiat! Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Amet deserunt mollitia quod error repellendus rem
+            labore enim esse sed velit! Sit harum facilis libero culpa a
+            provident sint animi voluptatem?
+          </h5>
+          <CTA />
+        </div>
         <div className="me">
           <img src={ME} alt="Niyongira James" />
         </div>
-
-        <a href="#contact" className="scroll__down">
-          Scroll Down
-        </a>
       </div>
     </header>
   );
