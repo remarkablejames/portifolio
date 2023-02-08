b12fd9f3f27b68b1341367970231dcadff777733
James Niyongira <lascasasjames85@gmail.com>
2023-02-07 23:41:15 -0500

    reinitializing project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,6 @@
 import React from "react";
-import { Header } from "./header/Header";
-import { Nav } from "./nav/Nav";
-import { About } from "./about/About";
-import { Experience } from "./experience/Experience";
-import { Services } from "./services/Services";
-import { Portifolio } from "./portifolio/Portifolio";
-import { Testimonials } from "./testimonials/Testimonials";
-import { Contact } from "./contact/Contact";
-import { Footer } from "./footer/Footer";
 
 const App = () => {
-  //   return (
-  //     <>
-  //       <Header />
-  //       <Nav />
-  //       <About />
-  //       <Experience />
-  //       <Portifolio />
-  //       <Testimonials />
-  //       <Contact />
-  //       <Footer />
-  //     </>
-  //   );
-
   return <h2> Site Under Construction. Come back soon </h2>;
 };
 
