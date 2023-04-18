99f647858d4a1d3345f768dcb5b0dc1420069458
James Niyongira <lascasasjames85@gmail.com>
2023-04-18 09:13:30 -0400

    added more content to the portfolio section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -17,52 +17,61 @@ export const Portfolio = () => {
                   </h1>
 
                   <div className=" text-white max-w-4xl mt-6 text-justify bg-transparent flex  space-x-6 p-12 ">
-                    <div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
-                      {/* img */}
-                      <img
-                        className="aspect-video w-96 rounded-t-2xl object-cover object-center"
-                        src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                        alt="img"
-                      />
-                      {/* text information */}
-                      <div className="p-4">
-                        <small className="text-blue-400 text-xs">
-                          Automobile company
-                        </small>
-                        <h1 className="text-2xl font-medium text-slate-600 pb-2">
-                          Dodge Car
-                        </h1>
-                        <p className="text-sm tracking-tight font-light text-slate-400 leading-6">
-                          Dodge is an American brand of automobiles and a
-                          division of Stellantis, based in Auburn Hills,
-                          Michigan..
-                        </p>
+                    <a
+                      href="https://natours-james.herokuapp.com"
+                      target="_blank"
+                    >
+                      <div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+                        {/* img */}
+                        <img
+                          className="aspect-video w-96 rounded-t-2xl object-cover object-center"
+                          src="https://images.unsplash.com/photo-1482332486572-10fff1bedfbe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+                          alt="img"
+                        />
+                        {/* text information */}
+                        <div className="p-4">
+                          <small className="text-blue-400 text-xs">
+                            A tour-booking web app
+                          </small>
+                          <h1 className="text-2xl font-medium text-slate-600 pb-2">
+                            Natours
+                          </h1>
+                          <p className="text-sm tracking-tight font-light text-slate-400 leading-6">
+                            Natours is a fullstack web application that let's
+                            your browse and book tours.
+                          </p>
+                        </div>
                       </div>
-                    </div>
+                    </a>
                     {/* ////////////////// */}
-                    <div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
-                      {/* img */}
-                      <img
-                        className="aspect-video w-96 rounded-t-2xl object-cover object-center"
-                        src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                        alt="img"
-                      />
-                      {/* text information */}
-                      <div className="p-4">
-                        <small className="text-blue-400 text-xs">
-                          Automobile company
-                        </small>
-                        <h1 className="text-2xl font-medium text-slate-600 pb-2">
-                          Dodge Car
-                        </h1>
-                        <p className="text-sm tracking-tight font-light text-slate-400 leading-6">
-                          Dodge is an American brand of automobiles and a
-                          division of Stellantis, based in Auburn Hills,
-                          Michigan..
-                        </p>
-                      </div>
-                    </div>
+                    <a
+                      href="https://notes-app-james.netlify.app/"
+                      target="_blank"
+                    >
+                      <div className="mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+                        {/* img */}
+                        <img
+                          className="aspect-video w-96 rounded-t-2xl object-cover object-center"
+                          src="https://images.unsplash.com/photo-1507925921958-8a62f3d1a50d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1752&q=80"
+                          alt="img"
+                        />
+                        {/* text information */}
 
+                        <div className="p-4">
+                          <small className="text-blue-400 text-xs">
+                            A notes-taking web app
+                          </small>
+                          <h1 className="text-2xl font-medium text-slate-600 pb-2">
+                            Reactive Notes
+                          </h1>
+                          <p className="text-sm tracking-tight font-light text-slate-400 leading-6">
+                            This is an application built on top of React and
+                            implements all the fundermental concepts of
+                            front-end.
+                          </p>
+                        </div>
+                      </div>
+                    </a>
                     {/* ////////////////// */}
                   </div>
                 </div>
